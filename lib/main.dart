3fe054347057df7c402e1d888874e1ce38fac742
Pehr95 <pehr.norten@gmail.com>
2025-02-11 22:53:02 +0100

    fixat färgen på overlay i android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_happy_plants_flutter/pages/login_page.dart';
 import 'package:my_happy_plants_flutter/pages/home_page.dart';
 import 'package:my_happy_plants_flutter/themes/light_mode.dart';
@@ -12,6 +13,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Fixa färgen på overlay i Android
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        systemNavigationBarColor: Color.fromARGB(255, 122, 162, 79),
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const LoginPage(),
