01a34d5023c1919e155173a3af1e62d57e6f3e13
FillePilsner1337 <144022686+FillePilsner1337@users.noreply.github.com>
2025-02-15 20:24:04 +0100

    Pytteliten update på easteregg

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -44,12 +44,12 @@ class _SettingsPageState extends State<SettingsPage> {
 
   void _initWebView() async {
     await _controller.initialize();
-    await _controller.loadUrl('https://www.youtube.com/embed/xvFZjo5PgG0');
+    await _controller.loadUrl('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
     setState(() => _isLoading = false);
   }
 
   void _fallbackLaunch() async {
-    const url = 'https://youtu.be/xvFZjo5PgG0';
+    const url = 'https://youtu.be/dQw4w9WgXcQ';
     if (await canLaunchUrl(Uri.parse(url))) {
       await launchUrl(Uri.parse(url));
     }
@@ -66,7 +66,7 @@ class _SettingsPageState extends State<SettingsPage> {
               borderRadius: BorderRadius.circular(15),
               boxShadow: [
                 BoxShadow(
-                    color: Colors.black.withOpacity(0.3),
+                    color: Colors.black,
                     spreadRadius: 3,
                     blurRadius: 7,
                     offset: const Offset(0, 3)),
