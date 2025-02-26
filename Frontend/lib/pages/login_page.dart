f86a88f196d652d72b2f5ee492e8929060717ee4
StorckC <storck-en@hotmail.com>
2025-02-26 15:49:44 +0100

    Finished all endpoint connections, can now run in android again.

--- Frontend/lib/pages/login_page.dart
+++ Frontend/lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_happy_plants_flutter/components/error_message_overlay.dart';
+import 'package:my_happy_plants_flutter/providers/authentication_provider.dart';
 import 'package:my_happy_plants_flutter/providers/login_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -14,6 +15,16 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _passwordVerifyController = TextEditingController();
+
+  bool _isSignUpMode = false;
+
+  void _toggleLoginMode() {
+    setState(() {
+      _isSignUpMode = !_isSignUpMode;
+    });
+  }
 
   void _login(BuildContext context) async {
     final loginProvider = context.read<LoginProvider>();
@@ -32,18 +43,39 @@ class _LoginPageState extends State<LoginPage> {
       return;
     }
 
-    //TODO: Här nere ligger en potentiell login metod, den är bortkommenterad för att jag inte har en login logik fixad.
-    /*
     final success = await loginProvider.login(email, password);
 
     if(success) {
       Navigator.pushNamed(context, '/home_page');
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Invalid credentials!")),
-      );
+      errorMessageOverlay(context, "Invalid credentials");
     }
-     */
+  }
+
+  void _signUp(BuildContext context) async {
+    final authProvider = context.read<AuthenticationProvider>();
+    final userName = _userNameController.text.trim();
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+    final confirmPassword = _passwordVerifyController.text.trim();
+
+    if(userName.isEmpty || email.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
+      errorMessageOverlay(context, "All fields must be filled");
+      return;
+    }
+
+    if(password != confirmPassword) {
+      errorMessageOverlay(context, "Password do not match");
+      return;
+    }
+
+    final success = await authProvider.createAccount(userName, email, password);
+
+    if(success) {
+      Navigator.pushNamed(context, '/login_page');
+    } else {
+      errorMessageOverlay(context, "Failed to create account");
+    }
   }
 
   @override
@@ -53,7 +85,6 @@ class _LoginPageState extends State<LoginPage> {
       body: Center(
         child: Container(
           width: 450,
-          height: 450,
           padding: const EdgeInsets.all(25),
           decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.primary,
@@ -63,32 +94,37 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                "Login",
+              Text(
+                _isSignUpMode ? "Create Account" : "Login",
                 style: TextStyle(
                   fontSize: 28,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               const SizedBox(height: 25),
+              if(_isSignUpMode)
+                _buildTextField("Username", Icons.person, _userNameController, false),
+
               _buildTextField("Email", Icons.email, _emailController, false),
-              const SizedBox(height: 15),
-              _buildTextField(
-                  "Password", Icons.lock, _passwordController, true),
+              _buildTextField("Password", Icons.lock, _passwordController, true),
+
+              if(_isSignUpMode)
+                _buildTextField("Confirm Password", Icons.lock, _passwordVerifyController, true),
               const SizedBox(height: 5),
+
+              if(!_isSignUpMode)
               Align(
                 alignment: Alignment.centerRight,
                 child: TextButton(
                   onPressed: () {
                     //TODO:  Add forgot password.
                   },
-                  child: const Text(
-                    "Forgot Password?",
-                    style: TextStyle(color: Colors.white70, fontSize: 14),
+                  child: const Text("Forgot Password?", style: TextStyle(color: Colors.white70, fontSize: 14),
                   ),
                 ),
               ),
               const SizedBox(height: 20),
+
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Theme.of(context).colorScheme.primary,
@@ -99,24 +135,15 @@ class _LoginPageState extends State<LoginPage> {
                   padding:
                       const EdgeInsets.symmetric(vertical: 14, horizontal: 50),
                 ),
-                onPressed: () => _login(
-                    context), // ------- Detta är troligtvis så login kommer funka senare.
-                //TODO: Här ska login funktionen vara. Den ska checka ditt lösenord osv. Nu kommer du alltid in.
+                onPressed: _isSignUpMode? () => _signUp(context) : () => _login(context), //Kommentera bort denna raden och lägg in den under om ni vill in.
                 //Navigator.pushNamed(context, '/home_page');
-
-                child: const Text(
-                  "Login",
-                  style: TextStyle(fontSize: 18),
+                child: Text(_isSignUpMode ? "Sign Up" : "Login", style: const TextStyle(fontSize: 18),
                 ),
               ),
-              const Spacer(),
+              const SizedBox(height: 20),
               TextButton(
-                onPressed: () {
-                  //TODO: Add create account logic.
-                },
-                child: const Text(
-                  "Create Account",
-                  style: TextStyle(color: Colors.white70, fontSize: 16),
+                onPressed: _toggleLoginMode,
+                child: Text(_isSignUpMode ? "Already have an account? Login" : "Create Account" , style: TextStyle(color: Colors.white70, fontSize: 16),
                 ),
               )
             ],
@@ -128,7 +155,9 @@ class _LoginPageState extends State<LoginPage> {
 
   Widget _buildTextField(String hint, IconData icon,
       TextEditingController controller, bool isPassword) {
-    return TextField(
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: TextField(
       controller: controller,
       obscureText: isPassword,
       decoration: InputDecoration(
@@ -140,6 +169,7 @@ class _LoginPageState extends State<LoginPage> {
           borderRadius: BorderRadius.circular(15),
           borderSide: BorderSide.none,
         ),
+      ),
       ),
     );
   }
